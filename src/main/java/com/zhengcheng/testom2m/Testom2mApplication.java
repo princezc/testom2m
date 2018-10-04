04f9579977afa2ff46c7ee6de52c61d0ce48fab3
836243070@qq.com <836243070@qq.com>
2018-10-04 11:56:18 +0800

    first commit

---
+++ src/main/java/com/zhengcheng/testom2m/Testom2mApplication.java
@@ -0,0 +1,12 @@
+package com.zhengcheng.testom2m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testom2mApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testom2mApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zhengcheng/testom2m/Testom2mApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhengcheng.testom2m;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Testom2mApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
